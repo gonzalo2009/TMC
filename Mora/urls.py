c383fd62d0e1ae6a959b30a4e719b8bf38a5813b
gonzalo2009 <42186601+gonzalo2009@users.noreply.github.com>
2020-06-29 20:18:31 -0400

    commit

--- Mora/urls.py
+++ Mora/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('https://tmc-chile.herokuapp.com/', include('TMC.urls')),
+    path('https://tmc-chile.herokuapp.com/TMC/', include('TMC.urls')),
     path('TMC/', include('TMC.urls')),
     path('admin/', admin.site.urls),
 ]
